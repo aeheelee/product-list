77da3a6c46363b271338f18347dee2b5a3b0c373
윤병진/콘텐츠프로덕트실 <byungjin.yoon@musinsa.com>
2023-12-26 19:36:21 +0900

    테스트

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import logo from '../assets/coz_logo_192.png';
+import logo from "../assets/coz_logo_192.png";
 
 export default function Header() {
   return (
     <header>
-      <div className='logo_container'>
-        <img alt='logo' src={logo} />
-        <h1>COZ SHOPPING</h1>
+      <div className="logo_container">
+        <img alt="logo" src={logo} />
+        <h1>COZ SHOPPING 123</h1>
       </div>
       <label>
-        <input placeholder='원하는 상품을 찾아보세요!' type='text' name='search' />
+        <input
+          placeholder="원하는 상품을 찾아보세요!"
+          type="text"
+          name="search"
+        />
         <span>
-          <i className='fa-solid fa-magnifying-glass'></i>
+          <i className="fa-solid fa-magnifying-glass"></i>
         </span>
       </label>
     </header>
