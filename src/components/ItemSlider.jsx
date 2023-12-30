3927f4158e25358b7ce8ca36a3b473875cf970a4
이애희/서치앤디스커버리개발팀 <ah.lee@musinsa.com>
2023-12-30 14:56:28 +0900

    feat: 슬라이드 스크립트 구현

--- src/components/ItemSlider.jsx
+++ src/components/ItemSlider.jsx
@@ -1,16 +1,92 @@
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import ItemCard from './ItemCard';
 
 const ItemSlider = ({ data }) => {
+  // const [sliderList, setSliderList] = useState({ start: 0, last: 5 });
+
+  // const itemSliderList =
+  //   sliderList.start < sliderList.last
+  //     ? data.slice(sliderList.start, sliderList.last)
+  //     : [
+  //         ...data.slice(sliderList.start, data.length),
+  //         ...data.slice(0, sliderList.last),
+  //       ];
+
+  // const prevButtonClick = () => {
+  //   setSliderList(({ last }) => {
+  //     const start = last === data.length ? 0 : last;
+
+  //     let nextLast = last - 5;
+
+  //     if (nextLast > data.length) {
+  //       nextLast = nextLast - data.length;
+  //     }
+
+  //     return { start, last: nextLast };
+  //   });
+  // };
+
+  // const nextButtonClick = () => {
+  //   setSliderList(({ last }) => {
+  //     const start = last === data.length ? 0 : last;
+
+  //     let nextLast = last + 5;
+
+  //     if (nextLast > data.length) {
+  //       nextLast = nextLast - data.length;
+  //     }
+
+  //     return { start, last: nextLast };
+  //   });
+  // };
+
+  const ref = useRef();
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const maxSlides = 5;
+
+  const updateMargin = () => {
+    ref.current.style.left = `${-currentSlide * 210}px`;
+  };
+
+  useEffect(updateMargin, [currentSlide]);
+
+  const isCheckActiveButton = (direction) => {
+    if (direction === 'prev') {
+      if (currentSlide === 0) {
+        alert('첫번째 아이템입니다');
+      }
+
+      return currentSlide >= 1;
+    } else if (direction === 'next') {
+      if (currentSlide === maxSlides) {
+        alert('마지막 아이템입니다');
+      }
+      return currentSlide < maxSlides;
+    }
+
+    return false;
+  };
+
+  const handleButtonClick = (direction) => {
+    if (isCheckActiveButton(direction)) {
+      setCurrentSlide((prev) => (direction === 'prev' ? prev - 1 : prev + 1));
+    }
+  };
+
   return (
     <StyledItemBox>
-      <StyledArrowLeft type="button">좌</StyledArrowLeft>
-      <StyledItemSlider>
+      <StyledArrowLeft type="button" onClick={() => handleButtonClick('prev')}>
+        &lt;
+      </StyledArrowLeft>
+      <StyledItemSlider ref={ref}>
         {data.map((item, index) => (
           <ItemCard key={index} data={item} setStyle={{ width: '200px' }} />
         ))}
       </StyledItemSlider>
-      <StyledArrowRight type="button">우</StyledArrowRight>
+      <StyledArrowRight type="button" onClick={() => handleButtonClick('next')}>
+        &gt;
+      </StyledArrowRight>
     </StyledItemBox>
   );
 };
@@ -35,19 +111,23 @@ const StyledItemSlider = styled.ul`
 
 const StyledArrowLeft = styled.button`
   position: absolute;
-  top: 0;
-  left: 0;
+  top: 29%;
+  left: 10px;
   color: white;
   font-size: 30px;
+  font-weight: bold;
+  line-height: 1;
   z-index: 1;
 `;
 
 const StyledArrowRight = styled.button`
   position: absolute;
-  top: 0;
-  right: 0;
+  top: 29%;
+  right: 10px;
   color: white;
+  font-weight: bold;
   font-size: 30px;
+  line-height: 1;
   z-index: 1;
 `;
 
